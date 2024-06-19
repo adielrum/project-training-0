aa696d22c3ccc8ec39ed50aa61a96328f412d7d1
adielrum <adiel.rum@sman2bdg.sch.id>
2024-06-19 23:20:20 +0700

    2nd push

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -2,3 +2,10 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+module.exports = {
+    reactStrictMode: true,
+    images: {
+      domains: ['your-domain.com'], // Add any domains you need for images
+    },
+  };    
