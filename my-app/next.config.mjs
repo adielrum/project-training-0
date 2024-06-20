903f7507377b86a075b130a1cb6a95dac573cb31
adielrum <adiel.rum@sman2bdg.sch.id>
2024-06-20 08:25:11 +0700

    minor fix 4

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -2,10 +2,3 @@
 const nextConfig = {};
 
 export default nextConfig;
-
-module.exports = {
-    reactStrictMode: true,
-    images: {
-      domains: ['your-domain.com'], // Add any domains you need for images
-    },
-  };    
